0d7a73a7599a88764cc86da9045e511a3b152f40
Owen G. Miller <111017418+OwenGMiller123@users.noreply.github.com>
2023-04-02 15:08:22 -0500

    Fixed typo

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ level_map = [
     '                             ',
     '                             ',
     ' XX     XXX          XX      ',
-    ' XX n                        ',
+    ' XX                          ',
     ' XXXX          XX         XX ',
     ' XXXX        XX              ',
     ' XX     X  XXXX    XX  XX    ',
