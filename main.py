a29bb0f4d84a18c095e08ada3fcbe46c2b83436e
Owen G. Miller <111017418+OwenGMiller123@users.noreply.github.com>
2023-05-13 16:14:38 -0500

    Update main.py
    Simple updates made for documentation purposes, specifically to tag the main game loop.

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import pygame, sys
 from settings import *
 from level import Level
 
-# Pygame setup
+# Pygame Setup #
 pygame.init()
 screen = pygame.display.set_mode((screen_width, 900))
 clock = pygame.time.Clock()
 level = Level(level_test_1, screen)
 
+# Main Game Loop #
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
