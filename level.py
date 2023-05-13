b68b5e3b4f830e1ee7f5a639b79adfc2df58be54
Owen G. Miller <111017418+OwenGMiller123@users.noreply.github.com>
2023-05-13 15:56:08 -0500

    Added simple GUI elements and rewrote horizontal collision function.
    1. Added some simple stat bar GUI elements for player's jump power and stamina. None of these stats do anything yet, but this tracks their behavior.
    
    2. Changed how horizontal collisions are determined. Instead of using player directionality and player state to determine how collisions are handled, instead now they are based on absolute values between the sides of the objects. This helps to eliminate some extremely troublesome bugs, and makes the function far, far simpler.

--- level.py
+++ level.py
@@ -20,7 +20,7 @@ class Level:
         self.scroll_threshold_rightx = 0.5
         
     def setup_level(self, layout):
-        """NOTES: This function loads the level layout and adds some additional Level class attributes.. The layout
+        """NOTES: This function loads the level layout and adds some additional Level class attributes. The layout
         argument can be specified by loading the appropriately named layout list from settings.py."""
         self.tiles = pygame.sprite.Group()
         self.player = pygame.sprite.GroupSingle()
@@ -44,57 +44,68 @@ class Level:
         self.player_camera.scroll_x_follow(self)
         self.player_camera.scroll_y_follow(self)
 
+    def draw_stat_bars(self):
+        """Draws the Player's Stats as bars. For now, this includes the player's jump power (green) and their stamina (yellow).
+        None of these stats function as of yet, so this is only for testing purposes for now."""
+        player = self.player.sprite
+        pygame.draw.rect(self.display_surface, (0, 255, 0), (10, 10, player.current_jump_power * 3, 10))
+        pygame.draw.rect(self.display_surface, (255, 255, 0), (10, 25, player.stamina, 10))
+
     def horizontal_movement_collision(self):
         """NOTES: This function controls horizontal collision with objects. If a collision is detected, the player
-        rectangle is snapped to the proper side of the object."""
+        rectangle is snapped to the proper side of the object.
+        
+        Directionality is determined by calculating the differnece in absolute value between the different sides of the
+        associated sprites. Specifically, the difference between player_right/sprite left and sprite_left/player
+        right. The smaller of the two determines which side the collision occured on, and snaps the player to that side
+        of the sprite. Doing so this way elimiates a lot of troublesome bugs that arise when collisions are handled by
+        checking player state and directionality, and greatly reduces function complexity."""
         player = self.player.sprite
+        player_state_x = player.player_state_x
         player.rect.x += player.direction.x * player.speed
 
         for sprite in self.tiles.sprites():
             if sprite.rect.colliderect(player.rect):
-
-                # sprite.image.fill('orange')                     # For testing purposes. Can be commented out.
-                if player.direction.x < 0:
-                    if player.direction.x < -12:
-                        player.rect.left = sprite.rect.right
-                        player.direction.x = 10
-                        self.world_shift_x = 0
-                    else:
-                        self.world_shift_x = 0
-                        player.direction.x = 0
-                        player.rect.left = sprite.rect.right
-
-                elif player.direction.x > 0:
-                    if player.direction.x > 12:
-                        player.rect.right = sprite.rect.left
-                        player.direction.x = -10
-                        self.world_shift_x = 0
-                    else:
-                        self.world_shift_x = 0
-                        player.direction.x = 0
-                        player.rect.right = sprite.rect.left
-
+                sprite.image.fill('orange')     # For collision type testing purposes. Can be commented out.
+                
+                if abs(player.rect.left - sprite.rect.right) < abs(player.rect.right - sprite.rect.left):
+                    # Left Collision #
+                    player.rect.left = sprite.rect.right
+                    player.direction.x = 0
+                    self.world_shift_x = 0
+                    player.player_state_x = 'colliding left'
+                else:
+                    # Right Collision #
+                    player.rect.right = sprite.rect.left
+                    player.direction.x = 0
+                    self.world_shift_x = 0
+                    player.player_state_x = 'colliding right'
+                print(f'Player State X: {player.player_state_x}')  # This is for testing purposes, can be commented out.
+                
     def vertical_movement_collision(self):
         """NOTES: This function handles vertical collision events. Generally, this allows the player to remain on the
-        ground without falling through."""
+        ground without falling through. It also handles collisions with the ceilings."""
         player = self.player.sprite
         player.apply_gravity()
 
         for sprite in self.tiles.sprites():
             if sprite.rect.colliderect(player.rect):
 
-                # sprite.image.fill('purple')                     # For testing purposes. Can be commented out.
+                sprite.image.fill('purple')                     # For testing purposes. Can be commented out.
                 if player.direction.y > 0:
+                    # Floor Collision #
                     player.rect.bottom = sprite.rect.top
                     player.direction.y = 0
                     player.on_ground = True
                     if player.current_jump_power < player.max_jump_power:
                         player.current_jump_power += player.jump_recharge_rate
-                        
                 if player.direction.y < 0:
+                    # Ceiling Collision #
                     player.rect.top = sprite.rect.bottom
                     player.direction.y = 0
-                    if self.world_shift_y > 0:
+                    if self.world_shift_y != 0:
+                        self.world_shift_y = 0
+                    if self.world_shift_y < 0:
                         player.rect.centery += self.world_shift_y
 
     def run(self):
@@ -106,6 +117,7 @@ class Level:
         self.tiles.draw(self.display_surface)
         self.level_camera()
         self.player.draw(self.display_surface)
+        self.draw_stat_bars()
 
         # Player
         self.player.update()
