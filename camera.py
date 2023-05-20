ff1d460b6a948ded5d4cc0ce80db8ea86ac94a46
Owen G. Miller <111017418+OwenGMiller123@users.noreply.github.com>
2023-05-20 17:40:37 -0500

    Fix CameraGroups
    Scrolling behavior is now handled within the Camera object rather than the Level object, which to me makes more sense.
    
    We've also changed scrolling behavior so that scrolling occurs on scrolling at any time rather than only when the player is touching the ground. This is more bug-prone, but feels a bit better depending. This also cuts down considerably on the complexity of camera functions, making everything more readable.
    
    Camera scrolling behavior is not set in stone here, and open to revision depending.

--- camera.py
+++ camera.py
@@ -1,141 +1,80 @@
-import pygame
-from tiles import Tile
-from settings import tile_size, screen_width, screen_height
-from player import Player
+from settings import screen_width, screen_height
 
 
 class CameraGroup:
     def __init__(self):
-        super().__init__()
-        self.tiles = pygame.sprite.Group()
 
-        # Screen Scrolling Attributes
+        # World Shift Variables #
         self.world_shift_x = 0
         self.world_shift_y = 0
-        self.scroll_threshold_leftx = 0.25
-        self.scroll_threshold_rightx = 0.5
+        self.scroll_threshold_left = 0.25
+        self.scroll_threshold_right = 0.5
+        self.scroll_threshold_top = 0.5
+        self.scroll_threshold_bottom = 0.9
+        
+    def snap_camera(self, target, map_layout, offset_x, offset_y):
+        """NOTES: This function snaps the camera to an object. It works by shifting the player sprite and level sprites
+        simultaneously by an offset that is calculated by the target object's original x/y coordinates (obj_x/y), and
+        the offset_x/y variables. These offset variables are percentages, and must be between 0 and 1 for the snap to
+        land within the display screen.
+        This function requires a target sprite (target), and the enumerated level layout (map_layout) so that the
+        object/level offsets can be applied appropriately."""
+        # Calculate Offsets #
+        cam_adjust_x = screen_width * offset_x
+        cam_adjust_y = screen_height * offset_y
+        level_shift_x = target.rect.centerx - cam_adjust_x
+        level_shift_y = target.rect.centery - cam_adjust_y
 
-    def center_camera_on_player(self):
-        """NOTES: This function centers the camera and the level on the player. Called when a level begins to place the
-        player appropriately on the screen. This prevents the player from being placed off screen at the beginning
-        of loading the level, forcing the screen to scroll to catch up with their position."""
-        player = self.player.sprite
+        # Center Object on Screen #
+        target.rect.centerx = cam_adjust_x
+        target.rect.centery = cam_adjust_y
 
-        player_x = player.rect.centerx
-        player_y = player.rect.centery
-        cam_adjust_x = screen_width * 0.25
-        cam_adjust_y = screen_height * 0.75
-        level_shift_x = player_x - cam_adjust_x
-        level_shift_y = player_y - cam_adjust_y
-
-        ### Center Player on Screen ###
-
-        player.rect.centerx = cam_adjust_x
-        player.rect.centery = cam_adjust_y
-
-        ### Shift Level to Player's Location ###
-
-        for sprite in self.tiles.sprites():
+        # Shift Level to Object's Location via Offset #
+        for sprite in map_layout:
             sprite.rect.centerx -= level_shift_x
             sprite.rect.centery -= level_shift_y
 
-        print('Centered from CameraGroup')      # This is for testing purposes to ensure that importing works properly.
-
-    def scroll_x_follow(self):
-        """NOTES: This function scrolls the screen horizonally as the player approaches the edge of a specified
-        threshold. These scrolling thresholds are controlled by the CameraGroup attributes self.scroll_threshold_leftx and
+        print('Centered from CameraGroup_Test')  # This is for testing purposes to ensure that importing works properly.
+        
+    def scroll_x_follow(self, target):
+        """NOTES: This function scrolls the screen horizontally as the player approaches the edge of a specified
+        threshold. These scrolling thresholds are controlled by the Level attributes self.scroll_threshold_leftx and
         self.scroll_threshold_rightx. They can be adjusted to meet the needs of a particular stage."""
+        target_center_x = target.rect.centerx
+        target_dir_x = target.direction.x
 
-        player = self.player.sprite
-        player_x = player.rect.centerx
-        direction_x = player.direction.x
-
-        speed_profile = player.speed_profile
-
-        if player_x < screen_width * self.scroll_threshold_leftx and direction_x < 0:
-            self.world_shift_x = -round(direction_x) * speed_profile
-            player.speed = 0
-        elif player_x > screen_width * self.scroll_threshold_rightx and direction_x > 0:
-            self.world_shift_x = -round(direction_x) * speed_profile
-            player.speed = 0
+        if target_center_x < screen_width * self.scroll_threshold_left and target_dir_x < 0:
+            self.world_shift_x = -round(target_dir_x)
+            target.speed = 0
+        elif target_center_x > screen_width * self.scroll_threshold_right and target_dir_x > 0:
+            self.world_shift_x = -round(target_dir_x)
+            target.speed = 0
         else:
             self.world_shift_x = 0
-            player.speed = 1
+            target.speed = 1
+            
+    def scroll_y_follow(self, target):
+        """NOTES: This function scrolls the screen horizontally as the target approaches the edge of a specified
+        threshold. This particular function follows the target at the scroll threshold in the direction they are
+        travelling to keep them within the inside of the screen.
+        Special adjustments are made for when the target reaches the threshold to adjust the to ensure that collision
+        bugs don't occur while the player is scrolling and landing/hitting the ceiling."""
+        # Bottom Screen Scrolling #
+        if target.rect.centery > screen_height * self.scroll_threshold_bottom:
+            if self.world_shift_y == 0:                         # Border Offset
+                target.rect.centery += target.direction.y
 
-    def scroll_y_follow(self):
-        """NOTES: This function scrolls the screen horizontally as the player approaches the edge of a specified
-        threshold. This is a comparatively complex function to avoid troublesome collision bugs that can arise due to
-        misalignments with the player/screen during scrolling."""
+            self.world_shift_y = -target.direction.y
+            target.rect.centery -= target.direction.y
 
-        player = self.player.sprite
-        player_y = player.rect.top
-        direction_y = player.direction.y
+        # Top Screen Scrolling #
+        elif target.rect.centery < screen_height * self.scroll_threshold_top and target.direction.y < 0:
+            if self.world_shift_y == 0:                         # Border Offset
+                target.rect.centery += target.direction.y
 
-        player_speed = player.speed
-        player_max_fall_speed = player.max_falling_speed
-        speed_profile = player.speed_profile
+            self.world_shift_y = -target.direction.y - 1
+            target.rect.centery -= target.direction.y
 
-        ###>>> Bottom Screen Scrolling <<<###
-
-        if player_y > screen_height * 0.75:
-            # The player is on the ground, below the scroll threshold. #
-            if player.on_ground is True:
-                if player_y > 0 and self.world_shift_y == 0:
-                    player.rect.centery += player.direction.y
-                if player_y > 0 and self.world_shift_y == -player_max_fall_speed:
-                    player.rect.centery += self.world_shift_y
-                    for sprite in self.tiles.sprites():
-                        sprite.rect.centery += player.direction.y
-
-                else:
-                    self.world_shift_y = -player_max_fall_speed
-
-            elif player.on_ground is False:
-                # The player is in the air, below the scroll threshold.
-                if self.world_shift_y <= 0:
-                    self.world_shift_y = 0
-                elif self.world_shift_y == 0:
-                    player.rect.centery += player.direction.y
-                    for sprite in self.tiles.sprites():
-                        sprite.rect.centery += player.direction.y
-
-                    self.world_shift_y = -player_max_fall_speed
-                    player.rect.centery -= player.direction.y
-
-        ###>>> Top Screen Scrolling <<<###
-
-        elif player_y < screen_height * 0.50:
-            # Player is in the air, and very high on the screen. #
-            if player.on_ground is False and player_y < screen_height * 0.25:
-                if player.direction.y < 0:
-                    if self.world_shift_y == 0:
-                        player.rect.centery += player.direction.y
-
-                    self.world_shift_y = -player.direction.y
-                    player.rect.centery -= player.direction.y
-
-                else:
-                    self.world_shift_y = 0
-
-            # Player is in the air, and somewhat high on the screen. #
-            elif player.on_ground is False and player_y > screen_height * 0.25:
-                self.world_shift_y = 0
-
-            # Player is on the ground, and high on the screen. #
-            elif player.on_ground is True:
-                if self.world_shift_y == 0:
-                    player.rect.centery += player.direction.y
-                if self.world_shift_y == player_max_fall_speed:
-                    player.rect.centery += player_max_fall_speed
-                    for sprite in self.tiles.sprites():
-                        sprite.rect.centery += player.direction.y
-
-                else:
-                    self.world_shift_y = player_max_fall_speed
-
-        ###>>> Player is Centered Within Scrolling Boundary <<<###
-
+        # Player Within Screen Scrolling Boundaries #
         else:
-            if self.world_shift_y != 0:
-                player.rect.centery += self.world_shift_y
             self.world_shift_y = 0
