1eb79b688b7dc3af8e3661041477591ea95b184d
Ossifier <111017418+Ossifier@users.noreply.github.com>
2023-07-30 23:08:35 -0500

    Update animate.py
    Added comments to animate_sprite_dict function.

--- animate.py
+++ animate.py
@@ -3,7 +3,11 @@ from spritesheets import SpriteSheet
 
 
 def animate_sprite_dict(sprite, state):
-    sprite.num_frames = len(sprite.data['animation states'][state]['framelist'])
+    """NOTES: Returns the next frame in the animation sequence for a sprite dictionary. Uses the sprite's loaded data,
+    along with its current state to check against the current number of frames that have elapsed since the frame
+    changed. If it is equal or larger, then it adds to the frame index, and resets the frame index back to zero if it is
+    larger than the length of the state frame list."""
+    sprite.num_frames = len(sprite.data['animationStates'][state]['frameList'])
 
     if sprite.current_time != 0 and sprite.current_time % sprite.animation_speed == 0:
         if sprite.current_frame >= sprite.num_frames - 1:
