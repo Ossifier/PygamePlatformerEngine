a74a6eb66a2088e7487830fb2e5263d63bc85c5d
Ossifier <111017418+Ossifier@users.noreply.github.com>
2023-08-23 19:14:25 -0500

    Added sprite flipping function.
    Added sprite flipping to animate module.

--- animate.py
+++ animate.py
@@ -2,6 +2,14 @@ import pygame
 from spritesheets import SpriteSheet
 
 
+def flip_img_xy(direction, sprite):
+    """NOTES: Flips the image based on which direction the entity is facing."""
+    if direction is 'right':
+        return sprite
+    else:
+        flipped_sprite = pygame.transform.flip(sprite, True, False)
+        return flipped_sprite
+
 def animate_sprite_dict(sprite, state):
     """NOTES: Returns the next frame in the animation sequence for a sprite dictionary. Uses the sprite's loaded data,
     along with its current state to check against the current number of frames that have elapsed since the frame
@@ -18,50 +26,3 @@ def animate_sprite_dict(sprite, state):
     sprite.current_time += 1
 
     return sprite.current_frame
-
-
-if __name__ == "__main__":
-
-    # BUILD SCREEN #
-    pygame.init()
-    DISPLAY_W, DISPLAY_H = 1000, 500
-    canvas = pygame.Surface((DISPLAY_W, DISPLAY_H))
-    window = pygame.display.set_mode((DISPLAY_W, DISPLAY_H))
-    clock = pygame.time.Clock()
-    FPS = 60
-
-    test_sprite = SpriteSheet('Sprites/full_sheet.png')
-    M_SPRITE = SpriteSheet('Sprites/full_sheet.png')
-
-    st_list = ['idle', 'running']
-
-    M_DICT = M_SPRITE.build_sprite_dict(st_list)
-
-    run = True
-    while run:
-
-        canvas.fill((50, 50, 50))
-
-        ### TEST ANIMATIONS ###
-        events = pygame.event.get()
-        for event in events:
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    print('K_UP')
-                    M_SPRITE.current_state = 'idle'
-                    M_SPRITE.current_frame = 0
-                    M_SPRITE.current_time = 0
-                if event.key == pygame.K_DOWN:
-                    print('K_DOWN')
-                    M_SPRITE.current_state = 'running'
-                    M_SPRITE.current_frame = 0
-                    M_SPRITE.current_time = 0
-
-            ### QUIT ###
-            if event.type == pygame.QUIT:
-                run = False
-
-        window.blit(canvas, (0, 0))
-        pygame.display.update()
-
-        clock.tick(FPS)
