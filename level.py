d525f6c24df3cf1015d207f88beb693b6ce40064
Ossifier <111017418+Ossifier@users.noreply.github.com>
2023-08-25 00:14:58 +0000

    Added to Debug/Toggle & Refactor
    Added to Debug and Toggle. Refactored player Atts.

--- level.py
+++ level.py
@@ -18,7 +18,10 @@ class Level:
         self.setup_level(level_data)
 
         # Screen Scrolling Attributes
-        self.level_camera_type = ''                # Camera selection for when multiple cameras are implemented.
+        self.level_camera_type = ''                 # Camera selection for when multiple cameras are implemented.
+
+        # Debug Panel
+        self.debug = False                          # For Toggling Debug Panel
         
     def setup_level(self, layout):
         """NOTES: This function loads the level layout and snaps the screen to the player's location. The layout
@@ -46,46 +49,61 @@ class Level:
         self.player_camera.scroll_y_follow(player_sprite)
 
     def draw_stat_bars(self):
-        """Draws the Player's Stats as bars. For now, this includes the player's jump power and their stamina."""
+        """NOTES: Draws the Player's Stats as bars. For now, this includes the player's jump power and their stamina."""
         player = self.player.sprite
         pygame.draw.rect(self.display_surface, (255, 255, 0), (10, 10, player.stamina, 10))
         pygame.draw.rect(self.display_surface, (0, 255, 0), (10, 25, player.current_jump_power * 3, 10))
 
     def draw_debug_panel(self):
+        """NOTES: Builds and displays debug panels for player movement, animation."""
         player = self.player.sprite
         font = pygame.font.SysFont('courier', 15)
 
         WHITE = (255, 255, 255)
         DK_GREY = (35, 35, 35)
 
-        fps = font.render(f'FPS: {round(self.fps, 3)}', True, WHITE)
-        pos_x = font.render(f'Pos X: {round(player.rect.centerx, 3)}', True, WHITE)
-        pos_y = font.render(f'Pos Y: {round(player.rect.centery, 3)}', True, WHITE)
-        dir_x = font.render(f'Dir X: {round(player.direction.x, 3)}', True, WHITE)
-        dir_y = font.render(f'Dir Y: {round(player.direction.y, 3)}', True, WHITE)
-        w_shft_x = font.render(f'Wld_Shft X: {round(self.player_camera.world_shift_x, 3)}', True, WHITE)
-        w_shft_y = font.render(f'Wld_Shft Y: {round(self.player_camera.world_shift_y, 3)}', True, WHITE)
-        state_x = font.render(f'State X: {player.player_state_x}', True, WHITE)
-        state_y = font.render(f'State Y: {player.player_state_y}', True, WHITE)
-        on_grnd = font.render(f'On Grnd: {player.on_ground}', True, WHITE)
-        jump = font.render(f'Jump: {player.jumping}', True, WHITE)
-        jump_pwr = font.render(f'Jump Pwr: {player.current_jump_power}', True, WHITE)
-        stam = font.render(f'Stamina: {player.stamina}', True, WHITE)
+        if self.debug is True: 
+            fps = font.render(f'FPS: {round(self.fps, 3)}', True, WHITE)
+            pos_x = font.render(f'Pos X: {round(player.rect.centerx, 3)}', True, WHITE)
+            pos_y = font.render(f'Pos Y: {round(player.rect.centery, 3)}', True, WHITE)
+            dir_x = font.render(f'Dir X: {round(player.direction.x, 3)}', True, WHITE)
+            dir_y = font.render(f'Dir Y: {round(player.direction.y, 3)}', True, WHITE)
+            w_shft_x = font.render(f'Wld_Shft X: {round(self.player_camera.world_shift_x, 3)}', True, WHITE)
+            w_shft_y = font.render(f'Wld_Shft Y: {round(self.player_camera.world_shift_y, 3)}', True, WHITE)
+            on_grnd = font.render(f'On Grnd: {player.on_ground}', True, WHITE)
+            jump = font.render(f'Jump: {player.jumping}', True, WHITE)
+            jump_pwr = font.render(f'Jump Pwr: {player.current_jump_power}', True, WHITE)
+            stam = font.render(f'Stamina: {player.stamina}', True, WHITE)
 
-        pygame.draw.rect(self.display_surface, DK_GREY, (940, 10, 250, 205))
-        self.display_surface.blit(fps, (950, 15))
-        self.display_surface.blit(pos_x, (950, 30))
-        self.display_surface.blit(pos_y, (950, 45))
-        self.display_surface.blit(dir_x, (950, 60))
-        self.display_surface.blit(dir_y, (950, 75))
-        self.display_surface.blit(w_shft_x, (950, 90))
-        self.display_surface.blit(w_shft_y, (950, 105))
-        self.display_surface.blit(state_x, (950, 120))
-        self.display_surface.blit(state_y, (950, 135))
-        self.display_surface.blit(on_grnd, (950, 150))
-        self.display_surface.blit(jump, (950, 165))
-        self.display_surface.blit(jump_pwr, (950, 180))
-        self.display_surface.blit(stam, (950, 195))
+            pygame.draw.rect(self.display_surface, DK_GREY, (940, 10, 250, 175))
+            self.display_surface.blit(fps, (950, 15))
+            self.display_surface.blit(pos_x, (950, 30))
+            self.display_surface.blit(pos_y, (950, 45))
+            self.display_surface.blit(dir_x, (950, 60))
+            self.display_surface.blit(dir_y, (950, 75))
+            self.display_surface.blit(w_shft_x, (950, 90))
+            self.display_surface.blit(w_shft_y, (950, 105))
+            self.display_surface.blit(on_grnd, (950, 120))
+            self.display_surface.blit(jump, (950, 135))
+            self.display_surface.blit(jump_pwr, (950, 150))
+            self.display_surface.blit(stam, (950, 165))
+
+            state_x = font.render(f'Ani State: {player.player_state}', True, WHITE)
+            p_dir = font.render(f'Face Dir: {player.player_facing_direction}', True, WHITE)
+            num_fr = font.render(f'Num Frms: {player.sprite_sheet.num_frames}', True, WHITE)
+            curr_fr = font.render(f'Curr Frm: {player.sprite_sheet.current_frame + 1}', True, WHITE)
+            curr_t = font.render(f'Curr Time: {player.sprite_sheet.current_time}', True, WHITE)
+            col_st_x = font.render(f'Col State X : {player.collision_state_x}', True, WHITE)
+            col_st_y = font.render(f'Col State Y: {player.collision_state_y}', True, WHITE)
+
+            pygame.draw.rect(self.display_surface, DK_GREY, (940, 195, 250, 115))
+            self.display_surface.blit(state_x, (950, 200))
+            self.display_surface.blit(col_st_x, (950, 215))
+            self.display_surface.blit(col_st_y, (950, 230))
+            self.display_surface.blit(p_dir, (950, 245))
+            self.display_surface.blit(num_fr, (950, 260))
+            self.display_surface.blit(curr_fr, (950, 275))
+            self.display_surface.blit(curr_t, (950, 290))
 
     def horizontal_movement_collision(self):
         """NOTES: This function controls horizontal collision with objects. If a collision is detected, the player
@@ -104,11 +122,11 @@ class Level:
                 if abs(player.rect.left - sprite.rect.right) < abs(player.rect.right - sprite.rect.left):
                     player.rect.left = sprite.rect.right
                     player.direction.x = 0
-                    player.player_state_x = 'colliding left'
-                else:
+                    player.collision_state_x = 'left'
+                elif abs(player.rect.left - sprite.rect.right) > abs(player.rect.right - sprite.rect.left):
                     player.rect.right = sprite.rect.left
                     player.direction.x = 0
-                    player.player_state_x = 'colliding right'
+                    player.collision_state_x = 'right'          
                     
     def vertical_movement_collision(self):
         """NOTES: This function handles vertical collision events. Generally, this allows the player to remain on the
@@ -128,11 +146,15 @@ class Level:
                     player.direction.y = 0
                     player.on_ground = True
                 if player.direction.y < 0:
+                    # Ceiling Collision #
                     player.rect.top = sprite.rect.bottom
                     if self.player_camera.world_shift_y != 0:    # Corrects ceiling collision bugs at scroll borders.
                         self.player_camera.world_shift_y = 0
                     player.direction.y = 0
-                    player.player_state_y = 'descending'
+                    player.collision_state_y = 'ceiling'
+                else:
+                    player.collision_state_y = 'none'
+
                     
     def run(self):
         """NOTES: This function runs the level and performs updates. Called in the main.py file. The order of functions
