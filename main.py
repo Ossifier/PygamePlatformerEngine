9afbd7bbd8c7e4570376876027556abe31b4e0ca
Ossifier <111017418+Ossifier@users.noreply.github.com>
2023-12-27 23:38:38 +0000

    Added FPS toggles, moved certain settings vals to settings.py
    1, 2, and 3 keys can now toggle different FPS for testing purposes.
    
    Calculating the interpolation multiplier (game_speed) can not be done in settings due to circular logic, so it is distributed from main.py. No sense in agonizing over this anymore, wasted enough time here, so I'll worry about it later.

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import pygame, sys, time
-from settings import *
+from maps import *
 from level import Level
+from settings import screen_width, screen_height, target_fps, game_speed
 
 # Pygame Setup #
 pygame.init()
-screen = pygame.display.set_mode((screen_width, 900))
+screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 level = Level(level_test_3, screen)
-FPS = 60
 
 previous_time = time.time()                                         # For Calculating Frame Interp
 
-# Main Game Loop #
+# Main Gameloop #
 while True:
     dt = time.time() - previous_time                                # For Calculating Frame Interp
     previous_time = time.time()                                     # For Calculating Frame Interp
-
     level.fps = clock.get_fps()                                     # For Debug Panel
     level.dt = dt                                                   # For Debug Panel / Distribute
+
+    level.game_speed = dt * game_speed
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -28,9 +29,15 @@ while True:
                 level.debug = True
             elif event.key == pygame.K_0 and level.debug is True:
                 level.debug = False
+            elif event.key == pygame.K_1:                           # Toggle FPS = 30 w/ 1 Key
+                target_fps = 30
+            elif event.key == pygame.K_2:                           # Toggle FPS = 60 w/ 2 Key
+                target_fps = 60
+            elif event.key == pygame.K_3:                           # Toggle FPS = 120 w/ 3 Key
+                target_fps = 120
 
     screen.fill('black')
     level.run()
 
     pygame.display.update()
-    clock.tick(FPS)
+    clock.tick(target_fps)
