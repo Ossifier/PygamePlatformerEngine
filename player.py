d1c271753af5e5918c8ce85768d527b17f59c658
Ossifier <111017418+Ossifier@users.noreply.github.com>
2023-12-27 23:41:45 +0000

    Large interpolation changes.
    Worked out some of the interpolation issues, began working on interpolations for animations.

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
-from spritesheets import SpriteSheet
 import animate
+from spritesheets import SpriteSheet
 
 
 class Player(pygame.sprite.Sprite):
@@ -12,7 +12,8 @@ class Player(pygame.sprite.Sprite):
         self.sprite_dict = self.sprite_sheet.build_sprite_dict(self.animation_states)
 
         # Player DT:
-        self.frame_correct = 0
+        self.game_speed = 1
+        # self.interp_mult = 0
         self.dt = 0
 
         # Player Animations
@@ -50,11 +51,11 @@ class Player(pygame.sprite.Sprite):
         self.player_facing_direction = 'right'
         self.jumping = False
         self.on_ground = True
-        self.running = False                        # Running applies a multiplier to run speed, costing stamina.
+        self.running = False                          # Running applies a multiplier to run speed, costing stamina.
         self.winded = False                           # 'Winded' simulates running out of breath while running/jumping.
 
         # Player Winded Stats:
-        self.winded_reset_threshold = 0.25           # % of stam bar that needs to be filled to clear Winded status.
+        self.winded_reset_threshold = 0.25              # % of stam bar that needs to be filled to clear Winded status.
         self.winded_stamina_recharge_penalty = 0.125    # % reduction of stam bar recharge on being Winded.
         self.winded_stamina_jump_penalty = 0.5
 
@@ -63,29 +64,24 @@ class Player(pygame.sprite.Sprite):
         momentum, falling, and the like. To be iterated upon later. This will likely get very complex.
         Additionally, the name may change later."""
         keys = pygame.key.get_pressed()
+        acc_spd_correct = self.move_acceleration * self.game_speed
 
-        # Running Right
-        if keys[pygame.K_RIGHT]:
-            if self.direction.x >= 0:
-                if self.direction.x >= self.max_move_speed:
-                    pass
-                else:
-                    self.direction.x += self.move_acceleration
-            elif self.direction.x <= 0:
-                self.direction.x += self.move_acceleration * 3
-
-        # Running Left
-        elif keys[pygame.K_LEFT]:
-            if self.direction.x <= 0:
-                if self.direction.x <= -self.max_move_speed:
-                    pass
-                else:
-                    self.direction.x -= self.move_acceleration
-            elif self.direction.x >= 0:
-                self.direction.x -= self.move_acceleration * 3
-
-        else:
-            if self.direction.x > 0.2:
+        if keys[pygame.K_RIGHT] and self.direction.x >= self.game_speed:      # Running Right
+            if self.direction.x >= self.max_move_speed:
+                pass
+            else:
+                self.direction.x += acc_spd_correct
+        elif keys[pygame.K_RIGHT] and self.direction.x <= self.game_speed:
+            self.direction.x += acc_spd_correct * 2
+        elif keys[pygame.K_LEFT] and self.direction.x <= self.game_speed:     # Running Left
+            if self.direction.x <= -self.max_move_speed:
+                pass
+            else:
+                self.direction.x -= acc_spd_correct
+        elif keys[pygame.K_LEFT] and self.direction.x >= self.game_speed:
+            self.direction.x -= acc_spd_correct * 2
+        else:                                                   # Coming To A Stop
+            if self.direction.x >= 0.2:
                 self.direction.x -= self.move_acceleration
             elif self.direction.x <= 0.2:
                 self.direction.x += self.move_acceleration
@@ -99,7 +95,8 @@ class Player(pygame.sprite.Sprite):
                 self.jump()
             elif self.on_ground is False and self.jumping is True and self.current_jump_power > 0:
                 self.jump()
-                self.current_jump_power -= 1
+                # self.current_jump_power -= 1
+                self.current_jump_power -= self.game_speed
             else:
                 self.jumping = False
 
@@ -110,9 +107,9 @@ class Player(pygame.sprite.Sprite):
         if keys[pygame.K_d] and self.winded is False:
             self.running = True
             if self.direction.x < self.max_running_speed and keys[pygame.K_RIGHT] is True:
-                self.direction.x += self.move_acceleration
+                self.direction.x += acc_spd_correct
             if self.direction.x > -self.max_running_speed and keys[pygame.K_LEFT] is True:
-                self.direction.x -= self.move_acceleration
+                self.direction.x -= acc_spd_correct
         else:
             self.running = False
 
@@ -152,7 +149,7 @@ class Player(pygame.sprite.Sprite):
             self.on_ground = False
             if self.on_ground is False and self.jumping is False:
 
-                animate.animate_sprite_slice(sprite, 6, 9)
+                animate.animate_sprite_slice(sprite, 6, 9)              # Move this inside animate fcn?
 
         # Jumping
         if self.on_ground is False and self.jumping is True or self.direction.y < 0:  # Jump or Dir for springs.
@@ -168,7 +165,7 @@ class Player(pygame.sprite.Sprite):
             self.direction.y = self.max_falling_speed
         else:
             self.direction.y += self.gravity
-        self.rect.y += self.direction.y * self.frame_correct
+        self.rect.y += self.direction.y * self.game_speed
 
         # print(f'PLAYER FALL SPEED - CORRECT: {self.direction.y * self.frame_correct}')
         # print(f'PLAYER FALL SPEED - DIR: {self.direction.y}')
@@ -182,9 +179,9 @@ class Player(pygame.sprite.Sprite):
         if self.on_ground is True and self.stamina > 0 and self.current_jump_power > 0:
             self.jumping = True
             self.on_ground = False
-            self.direction.y = self.jump_speed
+            self.direction.y = self.jump_speed + self.game_speed         # THIS WORKS???
         elif self.current_jump_power > 0 and self.winded is False:
-            self.direction.y = self.jump_speed
+            self.direction.y = self.jump_speed + self.game_speed         # THIS WORKS???
 
         # Set Jumping to False if Jump Power Depleted.
         if self.current_jump_power <= 1:
@@ -233,8 +230,13 @@ class Player(pygame.sprite.Sprite):
     def jump_power_handler(self):
         """NOTES This function handles jump power recharging mechanics. Basically, whenever the player is on the ground,
         the maximum height the can jump recharges over time up to a limit. """
-        if self.current_jump_power < self.max_jump_power and self.on_ground is True:
-            self.current_jump_power += self.jump_recharge_rate
+        if self.current_jump_power > self.max_jump_power:                               # Value Overflow Handling.
+            self.current_jump_power = self.max_jump_power
+        elif self.current_jump_power < 0:
+            self.current_jump_power = 0
+
+        if self.current_jump_power < self.max_jump_power and self.on_ground is True:    # Recharge Stamina on Ground.
+            self.current_jump_power += self.jump_recharge_rate * self.game_speed
 
     def animate_player(self):
         """NOTES: Animates the player sprite using the sprite_sheet dictionary and the player state as dictionary
@@ -244,7 +246,8 @@ class Player(pygame.sprite.Sprite):
         # Retrieve Current Sprite Frame #
         self.sprite_sheet.current_frame = animate.animate_sprite_dict(
             self.sprite_sheet,
-            self.player_state)
+            self.player_state,
+            self.game_speed)
 
         # Flip image based on direction and animate. #
         self.image = animate.flip_img_xy(player_sprite, self.player_facing_direction)
@@ -258,5 +261,4 @@ class Player(pygame.sprite.Sprite):
         self.jump_power_handler()
         self.animate_player()
 
-        print(f'Player Jump State: {self.jumping}')
-        
+        # print(f'Player Jump State: {self.jumping}')
