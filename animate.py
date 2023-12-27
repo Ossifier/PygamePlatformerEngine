0cb1c280b107d238d353d608a523b63f6d7e671c
Ossifier <111017418+Ossifier@users.noreply.github.com>
2023-12-27 23:43:52 +0000

    Tested animation function, WIP
    Working with variable game speed is going to require an algorithm to smooth out the animation, but this works for now.

--- animate.py
+++ animate.py
@@ -25,12 +25,15 @@ def animate_sprite_slice(sprite, start_fr, end_fr):
         sprite.current_time = 0
 
 
-def animate_sprite_dict(sprite, state):
+def animate_sprite_dict(sprite, state, game_speed):
     """NOTES: Returns the next frame in the animation sequence for a sprite dictionary. Uses the sprite's loaded data,
     along with its current state to check against the current number of frames that have elapsed since the frame
     changed. If it is equal or larger, then it adds to the frame index, and resets the frame index back to zero if it is
     larger than the length of the state frame list."""
     sprite.num_frames = len(sprite.data['animationStates'][state]['frameList'])
+
+    print(f'Num Frames: {sprite.num_frames}')
+    print(f'Frame Advance Speed: {game_speed}')
 
     if sprite.current_time != 0 and sprite.current_time % sprite.animation_speed == 0:
         if sprite.current_frame >= sprite.num_frames - 1:
@@ -38,6 +41,6 @@ def animate_sprite_dict(sprite, state):
         sprite.current_frame += 1
         sprite.current_time = -1
 
-    sprite.current_time += 1
+    sprite.current_time += round(game_speed)                    # NEEDS BETTER ALGORITHM, but it sort of works for now.
 
     return sprite.current_frame
